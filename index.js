daf793e7ce4aadbec2769b5ba347263383ea65aa
ITs-Me48 <juniorguibontzdumornay@gmail.com>
2024-11-13 18:24:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const Crypto = require('crypto')
 const path = require('path')
 const prefix = config.PREFIX
 
-const ownerNumber = ['923096287432']
+const ownerNumber = ['2397445120']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
